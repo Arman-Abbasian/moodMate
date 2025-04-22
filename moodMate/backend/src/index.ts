b551b374a7bfd62d49e0250b2fcbdb954bc22cf5
abbasian <abbasian@thn.com>
2025-04-22 18:02:47 +0330

    some changes

--- moodMate/backend/src/index.ts
+++ moodMate/backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import moodRoutes from './routes/mood'
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(cors())
 app.use(express.json())
-
+app.use('/static', express.static(path.join(__dirname, '..', 'public')))
 app.use('/api/mood', moodRoutes)
 
 app.listen(port, () => {
